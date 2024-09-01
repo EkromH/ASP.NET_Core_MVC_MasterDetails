a11827f2bc91a6568791882fc2b50a289eda196d
Ekrom Hossan <ekromfiverr@gmail.com>
2024-09-01 14:05:26 +0900

    Add project files.

---
+++ Data/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MasterDetailsInCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MasterDetailsInCore.Data
+{
+    public class MyDbContext: DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Applicant> Applicants { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+    }
+}
---
+++ Migrations/20240508032813_F99.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MasterDetailsInCore.Migrations
+{
+    public partial class F99 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
